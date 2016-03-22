fbc9744e10e0967de8484e3de8344fd9c22a5ccc
Blaise Ritchie <britchie@ellumus.com>
2016-03-22 13:07:31 -0700

    Added friendly from name to invite emails

--- util.py
+++ util.py
@@ -14,7 +14,7 @@ def randString(length):
 	return ''.join(random.SystemRandom().choice(string.letters + string.digits) for x in range(length))
 
 def sendEmail(toaddr, subject, body):
-	fromaddr = settings.EMAILUSER
+	fromaddr = settings.EMAILFROM
 
 	qm = QueMail.get_instance()
 	qm.send(Email(subject=subject, text=body, adr_to=toaddr, adr_from=fromaddr, mime_type='html'))
