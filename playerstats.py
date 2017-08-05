cc58b55598f326e7cbee38b3bdf7812b56b1a80f
John Canning <jmcanning@alum.mit.edu>
2017-08-04 17:58:44 -0700

    Templated playerstats and added stats for different periods
    Changed playerstats to have data handler that returns the stats data in
    JSON to use the Mustache templater to fill in each table of stats.
    
    Changed behavior of POST to playerstats to not do a redirect when values
    haven't changed.
    
    Added some utility functions for managing quarter names and computing
    previous and next quarters.

--- playerstats.py
+++ playerstats.py
@@ -1,9 +1,74 @@
 #!/usr/bin/env python3
 
+import json
 import db
 import handler
+from util import *
 
-class PlayerStats(handler.BaseHandler):
+class PlayerStatsDataHandler(handler.BaseHandler):
+    def get(self, player):
+        with db.getCur() as cur:
+            name = player
+            cur.execute("SELECT Id,Name,MeetupName FROM Players WHERE Id = ? OR Name = ?", (player, player))
+            player = cur.fetchone()
+            if player is None or len(player) == 0:
+                self.write(json.dumps({'status': 1,
+                                       'error': "Couldn't find player"}))
+                return
+            playerID, name, meetupName = player
+            statquery = """SELECT Max(Score),MIN(Score),COUNT(*),
+               ROUND(SUM(Score) * 1.0/COUNT(*) * 100) / 100,
+               ROUND(SUM(Rank) * 1.0/COUNT(*) * 100) / 100,
+               MIN(Rank), MAX(Rank), MIN(Date), MAX(Date), 
+               MIN(Quarter), MAX(Quarter)
+               """
+            fields = ['maxscore', 'minscore', 'numgames', 'avgscore',
+                      'avgrank', 'maxrank', 'minrank', 'mindate', 'maxdate',
+                      'minquarter', 'maxquarter']
+            N = 5
+            periods = [
+                {'name': 'All Time Stats',
+                 'subquery': "FROM Scores WHERE PlayerId = ?",
+                 'params': (playerID,)
+                },
+            ]
+            p = periods[0]
+            cur.execute(statquery + p['subquery'], p['params'])
+            p.update(dict(zip(fields,
+                              map(lambda x: round(x, 2) if isinstance(x, float) else x,
+                                  cur.fetchone()))))
+            if p['numgames'] == 0:
+                return self.render("playerstats.html", name=name,
+                                   error = "Couldn't find any scores for")
+            
+            # Add optional periods if warranted
+            if p['numgames'] > N:
+                periods.append(
+                    {'name': 'Last {0} Game Stats'.format(N),
+                     'subquery': "FROM (SELECT * FROM Scores WHERE PlayerId = ? ORDER BY Date DESC LIMIT ?)",
+                     'params': (playerID, N)
+                     })
+            if p['minquarter'] < p['maxquarter']:
+                periods.append(
+                    {'name': 'Quarter {0} Stats'.format(p['maxquarter']),
+                     'subquery': "FROM Scores WHERE PlayerId = ? AND Quarter = ?",
+                     'params': (playerID, p['maxquarter'])
+                     })
+                prevQtr = formatQuarter(prevQuarter(parseQuarter(p['maxquarter'])))
+                periods.append(
+                    {'name': 'Quarter {0} Stats'.format(prevQtr),
+                     'subquery': "FROM Scores WHERE PlayerId = ? AND Quarter = ?",
+                     'params': (playerID, prevQtr)
+                     })
+            for p in periods[1:]:
+                cur.execute(statquery + p['subquery'], p['params'])
+                p.update(dict(zip(fields,
+                                  map(lambda x: round(x, 2) if isinstance(x, float) else x,
+                                      cur.fetchone()))))
+            self.write(json.dumps({'playerstats': periods}))
+
+        
+class PlayerStatsHandler(handler.BaseHandler):
     def get(self, player):
         with db.getCur() as cur:
             name = player
@@ -14,34 +79,12 @@ class PlayerStats(handler.BaseHandler):
                                    error = "Couldn't find player")
 
             player, name, meetupname = player
-            cur.execute("""SELECT Max(Score),MIN(Score),COUNT(*),
-               ROUND(SUM(Score) * 1.0/COUNT(*) * 100) / 100,
-               ROUND(SUM(Rank) * 1.0/COUNT(*) * 100) / 100,
-               MIN(Date), Max(Date)
-               FROM Scores WHERE PlayerId = ?""", (player,))
-            maxscore, minscore, numgames, avgscore, avgrank, mindate, maxdate =\
-                map(lambda x: round(x, 2) if isinstance(x, float) else x,
-                    cur.fetchone())
-            cur.execute("""
-              SELECT ROUND(Sum(Score) * 1.0 / COUNT(*) * 100) / 100,
-              ROUND(Sum(Rank) * 1.0 / COUNT(*) * 100) / 100
-              FROM (SELECT * FROM Scores WHERE PlayerId = ?
-                    ORDER BY Date DESC LIMIT 5)""", (player,))
-            avgscore5, avgrank5 = cur.fetchone()
             self.render("playerstats.html",
                         error = None,
                         name = name,
                         meetupname = meetupname,
-                        maxscore = maxscore,
-                        minscore = minscore,
-                        numgames = numgames,
-                        avgscore = avgscore,
-                        avgrank  = avgrank,
-                        avgscore5 = avgscore5,
-                        avgrank5 = avgrank5,
-                        mindate = mindate,
-                        maxdate = maxdate
                 )
+
     def post(self, player):
         name = self.get_argument("name", player)
         meetupname = self.get_argument("meetupname", None)
@@ -59,4 +102,4 @@ class PlayerStats(handler.BaseHandler):
                 args += [player, player]
                 with db.getCur() as cur:
                     cur.execute(query, args)
-        self.redirect("/playerstats/" + name)
+            self.redirect("/playerstats/" + name)
