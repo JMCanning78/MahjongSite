3110de990cf381dff4084b8a2c9e14a0cbdd9560
John Canning <jmcanning@alum.mit.edu>
2017-10-11 10:45:35 -0700

    Update beautify.sh to accept specific files on command line
    Allow beautify.sh to operate on specific files or every javascript
    file in the static/js directory.  Also change the command line
    syntax so it works with both the python and node.js versions of
    js-beautify.  NOTE: this requires dropping the -p argument to
    js-beautify which is not documented in the command line help and
    is only accepted by the node.js version of js-beautify.  I am
    not sure what the -p flag is supposed to do.

--- beautify.sh
+++ beautify.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-js-beautify -tnprb end-expand static/js/*
+if [ $# -eq 0 ] ; then
+    for f in static/js/*.js ; do
+	js-beautify -tnr --brace-style=end-expand $f
+    done
+else
+    for f in $* ; do
+	js-beautify -tnr --brace-style=end-expand $f
+    done
+fi
+
