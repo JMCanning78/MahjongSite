dcadd12152e92fcc351d7214b698984623eae8e9
Blaise Ritchie <ritchie.blaise@gmail.com>
2018-04-18 18:23:30 -0700

    Fixed leaderboard generation and username styling

--- leaderboard.py
+++ leaderboard.py
@@ -133,7 +133,7 @@ def genLeaderboard(leaderDate = None):
                 bindings = [leaderDate] * datefmt.count("{date}")
             else:
                 datetest = "1"
-            cur.execute("DELETE FROM Leaderboards WHERE Period = ? AND {datetest}".format(datetest=datetest), [periodname] + bindings)
+            cur.execute("DELETE FROM Leaderboards WHERE Period = ? AND Date = {datefmt}".format(datefmt=datefmt.format(date="?")), [periodname] + bindings)
 
             for query in queries:
                 cur.execute(query.format(datetest=datetest, datefmt=datefmt).format(date="Scores.Date"), [scores.getUnusedPointsPlayerID()] + bindings)
