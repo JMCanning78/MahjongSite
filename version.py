dac96b72e59b7d817e873d3b5e289c9cfa68a3db
John Canning <jmcanning@alum.mit.edu>
2018-08-16 15:56:29 -0700

    Make version tolerate missing pygit2 module
    It is hard to install pygit2 on some systems since it requires other
    modules and C++ compiliation.  For those systems, the /version endpoint
    still runs but returns an "unknown commit".

--- version.py
+++ version.py
@@ -2,7 +2,23 @@
 
 import handler
 import os.path
-from pygit2 import Repository
+try:
+    from pygit2 import Repository
+except:
+    import collections
+    authorRecord = collections.namedtuple('Author', 'name, email')
+    committerRecord = collections.namedtuple('Committer', 'name, email')
+    nodeRecord = collections.namedtuple('Node', 
+                                        'id, message, author, committer')
+    class Node(object):
+        def get_object(self):
+            return nodeRecord("unknown commit",
+                              "Missing pygit2 to determine commit",
+                              authorRecord("unknown", ""),
+                              committerRecord("unknown", ""))
+    class Repository(object):
+        def __init__(self, filename=None):
+            self.head = Node()
 
 class VersionHandler(handler.BaseHandler):
     def get(self):
