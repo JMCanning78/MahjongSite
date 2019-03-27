31456bd4aa14b80912a74a1cf15e03cdf8bafb00
Blaise Ritchie <ritchie.blaise@gmail.com>
2019-03-26 21:41:37 -0700

    Fixed beautify script for next commit

--- beautify.sh
+++ beautify.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ $# -eq 0 ] ; then
-	js-beautify -tnr --brace-style=end-expand \
-		`ls static/js/*.js | fgrep -v .min.js`
+  find static/js \( -name "*.js" -and -not -iname '*.min.js' \) -exec \
+    js-beautify -tnr --brace-style=end-expand {} \;
 else
 	js-beautify -tnr --brace-style=end-expand $*
 fi
